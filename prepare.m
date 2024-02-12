70223cb20e1a243aa7270658afb48760eea94ec8
xliu <xun.liu.1991@gmail.com>
2024-02-12 14:41:07 -0600

    adjust perturbation calculation

--- prepare.m
+++ prepare.m
@@ -14,7 +14,7 @@ xeq = original_data.full_xeq;
 yeq = original_data.full_yeq;
 
 % calculate the perturbation.
-perturbation = density_upper_bond - density_lower_bond;
+ perturbation = 2 * (density_upper_bond - density_lower_bond)./(density_upper_bond + density_lower_bond);
 
 % interporlation on the omni data.
 ae_index = interp1(original_data.partial_epoches, ...
@@ -25,8 +25,20 @@ sym_h = interp1(original_data.partial_epoches, ...
     original_data.partial_sym_h, ...
     original_data.full_epoches);
 
-
 % collect into table.
-variable_names = {'time', 'mlat', 'xeq', 'yeq', 'ae_index', 'sym_h', 'perturbation'};
-table = table(t, mlat, xeq, yeq, ae_index, sym_h, perturbation, 'VariableNames', variable_names);
-writetable(table, 'data.csv');
+variable_names = {'time', 'mlat', 'cos', 'sin', 'rho', 'ae_index', 'sym_h', 'perturbation'};
+[theta, rho] = cart2pol(xeq, yeq);
+cs = cos(theta);
+sn = sin(theta);
+
+table = table(t', mlat', cs', sn', rho', ...
+    ae_index', sym_h', ...
+    perturbation', 'VariableNames', variable_names);
+
+% use sub selected data to reduce data size.
+selection_rate = 1/1000;
+sz = length(t);
+row_indexes = randperm(sz, int32(sz*selection_rate));
+subtable = table(row_indexes, :);
+
+% writetable(subtable, 'dataf1000.csv', 'WriteVariableNames', true);
